e608aa4f2c8a2a3567ec9ea52dd54f4c41d9d04f
Arisabeth Simphoukham <rizsim@gmail.com>
2019-05-08 14:07:41 -0700

    pics, navbar and landing page

--- multipagewebsite/src/app/app.module.ts
+++ multipagewebsite/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPgComponent } from './landing-pg/landing-pg.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPgComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
